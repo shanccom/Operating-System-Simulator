30ec73cf18c8e5d760050d26e2b4788df01a5dda
Sergio Danilo Hancco Mullisaca <shanccom@unsa.edu.pe>
2025-11-23 12:32:35 -0500

    Estados de un Proceso

--- model/ProcessState.java
+++ model/ProcessState.java
@@ -1,1 +1,22 @@
-######
+package model;
+
+/**
+ * Estados de un proceso
+**/
+
+public enum ProcessState {
+  NEW,
+  READY,
+  RUNNING,
+  BLOCKED_MEMORY,
+  BLOCKED_IO,
+  TERMINATED;
+
+  public boolean isBlocked() {
+      return this == BLOCKED_MEMORY || this == BLOCKED_IO;
+  }
+  
+  public boolean isActive() {
+      return this != TERMINATED;
+  }
+}
