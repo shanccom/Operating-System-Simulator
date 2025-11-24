e061492d6a118c499315adde17be9dabe3c62d3f
Sergio Danilo Hancco Mullisaca <shanccom@unsa.edu.pe>
2025-11-24 00:12:06 -0500

    Metodo mainExecutionLoop el cual es la parte mas importante porque aca es donde el proceso espera su turno, se ejecuta cuano le toca y se bloquea cuando necesita algo

--- modules/sync/ProcessThread.java
+++ modules/sync/ProcessThread.java
@@ -21,4 +21,82 @@ public class ProcessThread extends Thread {
     Logger.log("Thread creado para proceso " + process.getPid());
   }
 
+  private void waitForArrival() throws InterruptedException {
+    int arrivalTime = process.getArrivalTime();
+    Logger.debug("Proceso " + process.getPid() + " esperando hasta tiempo " + arrivalTime);
+
+    while(running && syncController.getScheluder().getCurrentTime() < arrivalTime) {
+      Thread.slepp(10);
+    }
+
+    Logger.debug("Proceso " + process.getPid() + " alcanzo su tiempo de llegada");
+  }
+
+  private void arriveAtSystem() {
+    Logger.log("Proceso " + process.getPid() " llego al sistema");
+    process.setState(ProcessState.READY);
+    syncController.notifyProcessReady(process);
+  }
+
+  private void mainExecutionLoop() throws InterruptedException{
+    while (running && !process.isCompleted()) {
+      waitForRunningState();
+
+      if (!running || process.getState() == ProcessState.TERMINATED){
+        break;
+      }
+
+      Burst currentBurst = process.getCurrentBurst();
+
+      if (currentBUrst == null) {
+        Logger.log("Proceso " + process.getPid() + " no tinee mas rafagas");
+        break;
+      }
+
+      if (currentBurst.isCPU()) {
+        executeCPUBurst(currentBurst);
+      } else {
+        executeIOBurst(currentBurst);
+      }
+
+      if (currentBurst.isCompleted()) {
+        process.advanceBurst();
+        Logger.debug("Proceso " + process.getPid() + " avanzo a la siguiente rafaga");
+      }
+    }
+    terminateProcess();
+  }
+
+  private void waitForRunningState() throws InterruptedException {
+    synchronized(process) {
+      while(running && process.getState() != ProcessState.RUNNING && process.getState() != ProcessState.TERMINATED){
+        Logger.debug("Proceso " + process.getPid() + " esperando turno (estado: " + process.getState() + ")");
+        process.wait();
+      }
+      Logger.debug("Proceso " + process.getPid() + " desperto (estado: " + process.getState() + ")");
+    }
+  }
+
+
+
+  @Override 
+  public void run() {
+    try {
+      Logger.log("Thread del proceso " + process.getPid() + " iniciado")
+
+      waitForArrival();
+      arriveAtSystem();
+      mainExecutionLoop();
+
+      Logger.log("Thread del proceso " + process.getPid() + " terminado");
+
+    } catch (InterruptedException e) {
+      Logger.warning("Thread del proceso + " process.getPid() + "interrumpido");
+      Thread.currentThread().interrupt();
+    }
+  }
+
+
+
+
 }
