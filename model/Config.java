9cf32392c17cec0d0084a7267be925d10ec8bc24
FabbPP <ffpacheco239@gmail.com>
2025-11-25 18:36:18 -0500

    Configuracion

--- model/Config.java
+++ model/Config.java
@@ -12,7 +12,7 @@ public class Config {
   public enum ReplacementType {
       FIFO,     // First In First Out
       LRU,      // Least Recently Used
-      OPTIMAL   // Algoritmo Óptimo
+      OPTIMAL,   // Algoritmo Óptimo
       NRU   // Algoritmo Óptimo
   }
   
