b3e3a15fc3a808b02cb9933630c34c802f555cb6
dmolloc1 <dmolloc@unsa.edu.pe>
2025-11-27 12:15:04 -0500

    feat(LogsPanel):Se integra uso de Logger

--- src/main/java/modules/gui/components/LogsPanel.java
+++ src/main/java/modules/gui/components/LogsPanel.java
@@ -2,9 +2,13 @@ package modules.gui.components;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
+import utils.Logger;
 
 public class LogsPanel extends VBox {
+
+    private final ListView<Logger.LogEntry> list = new ListView<>();
 
     public LogsPanel() {
         setSpacing(10);
@@ -13,7 +17,12 @@ public class LogsPanel extends VBox {
 
         Label title = new Label("Panel de Ejecución");
         title.getStyleClass().add("card-title");
-        getChildren().add(title);
-    }
 
+        getChildren().addAll(title, list);
+
+        Logger.addListener(entry -> {
+            list.getItems().add(entry);   
+            list.scrollTo(list.getItems().size() - 1);
+        });
+    }
 }
