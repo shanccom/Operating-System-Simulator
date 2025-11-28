4653262ba1968e993925c11e84bd82af4d3cd1f5
dmolloc1 <dmolloc@unsa.edu.pe>
2025-11-28 13:40:04 -0500

    feat(ResultadosPage): estructura inicial de la pagina

--- src/main/java/modules/gui/pages/ResultadosPage.java
+++ src/main/java/modules/gui/pages/ResultadosPage.java
@@ -1,12 +1,45 @@
 package modules.gui.pages;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import model.ResultadoProceso;
 
-public class ResultadosPage extends VBox{
+public class ResultadosPage extends VBox {
+
     public ResultadosPage() {
-        Label title = new Label("Resultados");
+        setSpacing(20);
+        setPadding(new Insets(20));
 
-        getChildren().add(title);
+        construirEncabezado();
+        construirTarjetas();
+        construirVisualizaciones();
+        construirTabla();
+    }
+
+    private void construirEncabezado() {
+        BorderPane barra = new BorderPane();
+        getChildren().add(barra);
+    }
+
+    private void construirTarjetas() {
+        GridPane grid = new GridPane();
+        grid.setHgap(12);
+        grid.setVgap(12);
+        getChildren().add(grid);
+    }
+
+    private void construirVisualizaciones() {
+        VBox seccion = new VBox(10);
+        getChildren().add(seccion);
+    }
+
+    private void construirTabla() {
+        TableView<ResultadoProceso> tabla = new TableView<>();
+        getChildren().add(tabla);
     }
 }
+
