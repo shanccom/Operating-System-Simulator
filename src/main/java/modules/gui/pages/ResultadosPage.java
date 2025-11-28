8ba676e8bc5c5816888e03f195151f2d79a23ea6
dmolloc1 <dmolloc@unsa.edu.pe>
2025-11-28 13:51:15 -0500

    feat(ResultadosPage): se construyo tabla de procesos finales

--- src/main/java/modules/gui/pages/ResultadosPage.java
+++ src/main/java/modules/gui/pages/ResultadosPage.java
@@ -6,10 +6,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+
 import model.ResultadoProceso;
 
 public class ResultadosPage extends VBox {
@@ -117,8 +120,27 @@ public class ResultadosPage extends VBox {
 
 
     private void construirTabla() {
+        Label titulo = new Label("Métricas por proceso");
+        titulo.getStyleClass().add("section-title");
+
         TableView<ResultadoProceso> tabla = new TableView<>();
-        getChildren().add(tabla);
+
+        TableColumn<ResultadoProceso, String> pidCol = new TableColumn<>("Proceso");
+        pidCol.setCellValueFactory(new PropertyValueFactory<>("pid"));
+
+        TableColumn<ResultadoProceso, Integer> esperaCol = new TableColumn<>("Espera (ms)");
+        esperaCol.setCellValueFactory(new PropertyValueFactory<>("tiempoEspera"));
+
+        TableColumn<ResultadoProceso, Integer> retornoCol = new TableColumn<>("Retorno (ms)");
+        retornoCol.setCellValueFactory(new PropertyValueFactory<>("tiempoRetorno"));
+        tabla.getColumns().addAll(pidCol, esperaCol, retornoCol);
+
+        tabla.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        tabla.getStyleClass().add("result-table");
+        tabla.getStyleClass().add("result-table");
+
+        getChildren().addAll(titulo, tabla);
     }
+
 }
 
